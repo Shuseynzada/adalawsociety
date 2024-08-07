1fa99c2777b05f6de00a89c54483023f3d717695
samxal <sh.samxal@gmail.com>
2024-08-07 12:26:46 +0400

    init

---
+++ public/assets/index.ts
@@ -0,0 +1,36 @@
+import placeholderImg from "./placeholder-image.png"
+import societybg from "./societybg.jpg"
+import azFlagIcon from "./az.png"
+import enFlagIcon from "./en.png"
+import mainLogo from "./Law society logo.png"
+import eventPicture1 from "./events/Screenshot 2024-02-11 at 17.25.19.png"
+import eventPicture2 from "./events/Screenshot 2024-02-11 at 17.27.12.png"
+import eventPicture3 from "./events/Screenshot 2024-02-11 at 17.43.40.png"
+
+import footerGallery1 from "./gallery/1.jpg"
+import footerGallery2 from "./gallery/2.jpg"
+import footerGallery3 from "./gallery/3.jpg"
+import footerGallery4 from "./gallery/4.jpg"
+import footerGallery5 from "./gallery/5.jpg"
+import footerGallery6 from "./gallery/6.jpg"
+import footerGallery7 from "./gallery/7.png"
+import footerGallery8 from "./gallery/8.png"
+
+export {
+    placeholderImg,
+    societybg,
+    azFlagIcon,
+    enFlagIcon,
+    mainLogo,
+    eventPicture1,
+    eventPicture2,
+    eventPicture3,
+    footerGallery1,
+    footerGallery2,
+    footerGallery3,
+    footerGallery4,
+    footerGallery5,
+    footerGallery6,
+    footerGallery7,
+    footerGallery8,
+}
