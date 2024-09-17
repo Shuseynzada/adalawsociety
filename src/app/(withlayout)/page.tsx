c98ff9a26cc373230247385e3d3689a37f5bc792
samxal <sh.samxal@gmail.com>
2024-09-17 23:04:32 +0400

    eslint error fix

--- src/app/(withlayout)/page.tsx
+++ src/app/(withlayout)/page.tsx
@@ -50,13 +50,13 @@ function About() {
         <p className="text-xl max-w-md">
           ADA Law Society (ALS) is a student organization established in
           September 2019. The Society organizes different events with the
-          purpose of extending students' knowledge and contributing to their
+          purpose of extending students&apos; knowledge and contributing to their
           student life simultaneously. By attending the events of the Society,
           students will get a chance to hear from distinguished local and
           foreign lawyers and law professors their experience and knowledge on
           certain fields of law. Topics of the events interest not only law
           students, but students of other majors as well. The scope of the
-          Society's activities includes, but not limited with, lectures, talks,
+          Society&apos;s activities includes, but not limited with, lectures, talks,
           legal discussions, academic excursions, moot court competitions.
         </p>
       </div>
